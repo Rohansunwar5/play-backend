276b1f9cf8dd3a77335fe653a1005d65c2e5b792
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-02-09 22:41:14 +0530

    connection ot mongo atlas

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// import and execute the .env file to the program because when the application is loaded env file should be the first one that should be available to everyone 
+
+// require('dotenv').config()
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: '../env'
+})
+// "scripts": {  in order to run above config 
+//   "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
+// },
+
+connectDB()
+
+/*
+import express from "express"
+const app = express()
+
+( async() => {
+  try {
+    await mongoose.connect (`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", () => {
+      console.log("ERROR: ", error);
+      throw error
+    })
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.error("ERROR: ", error)
+    throw err
+  }
+}) ()
+*/
