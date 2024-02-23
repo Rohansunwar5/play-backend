473bd312f9d56cff9a57a03c95b95f4528c2cdac
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-02-23 23:42:36 +0530

    Add API to get user channel profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -341,6 +341,70 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
     new ApiResponse(200, user, "Cover Image updated successfully ")
   )
 })
+
+const getUserChannelProfile = asyncHandler(async(req,res) => {
+  const {username} = req.params  // as this is taken from thr url not from the body
+
+  if(!username){
+    throw new ApiError(400,'username is missing')
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match : {
+        username : username?.toLowerCase(),
+
+      }
+    },
+    {
+      $lookup : {
+        from : "subscriptions", //  coming from subscription model or db , its stored in plural that's why we are using this field name 
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup:{
+        from : "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: { // additional fields add up
+          subscribersCount: {
+            $size:"$subscribers"
+          },
+          channelsSubscribedToCount: {
+            $size: "$subscribedTo"
+          },
+          isSubscribed: {
+            $cond: {
+              if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+              then: true,
+              else: false
+            }
+          }
+      }
+    },
+    {
+      $project: {// projection  removes unnecessary data, give only selected data 
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      }
+    }
+  ])
+
+})
+
 export {
   registerUser,
   loginUser,
@@ -351,4 +415,5 @@ export {
   updateAccountDetails,
   updateUserCoverImage,
   updateUserAvatar,
+  getUserChannelProfile,
 } 
